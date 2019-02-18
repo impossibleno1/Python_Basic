993ebb17e06bca7d92ef5728d58373fc89da758e
impossibleno1 <37857357+impossibleno1@users.noreply.github.com>
2019-02-18 15:26:37 +0700

    EX1:Character Input

---
+++ Day1_Ex1-5/Exercise1.py
@@ -0,0 +1,13 @@
+#Yeu cau nhap ten
+name = input("Vui long nhap ten: ")
+print("Ten cua ban la : " + name )
+
+#Yeu cau nhap tuoi
+age = int(input("Vui long nhap so tuoi: "))
+
+#Tinh nam sinh
+bnyear = 2019 - age
+
+#Tinh so nam dat 100 tuoi
+year100 = bnyear + 100
+print("So nam ban dat 100 tuoi la: " + str(year100))
